bbdd1e57e950924771270207df62f3eb8a2fd986
Howard <howardn@egb074000017.lancs.local>
2020-02-10 21:26:13 +0000

    init

---
+++ trace.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import socket
+import os
+import sys
+import struct
+import time
+import select
+import binascii  
+
+
+ICMP_ECHO_REQUEST = 8 #ICMP type code for echo request messages
+ICMP_ECHO_REPLY = 0 #ICMP type code for echo reply messages
+MAX_HOPS = 30
+PORT = 33434
+
+def checksum(string): 
+    str_ = bytearray(string)
+    csum = 0
+    countTo = (len(str_) // 2) * 2
+
+    for count in range(0, countTo, 2):
+        thisVal = str_[count+1] * 256 + str_[count]
+        csum = csum + thisVal
+        csum = csum & 0xffffffff
+
+    if countTo < len(str_):
+        csum = csum + str_[-1]
+        csum = csum & 0xffffffff
+
+    csum = (csum >> 16) + (csum & 0xffff)
+    csum = csum + (csum >> 16)
+    answer = ~csum
+    answer = answer & 0xffff
+    answer = answer >> 8 | (answer << 8 & 0xff00)
+    return answer
+    
+def receiveOnePing(icmpSocket, destinationAddress, ID, timeout, timeSent):
+    curr_addr = None
+    curr_host = None
+    tries = 3
+    done = False
+
+    '''
+    while not done and tries > 0:
+        try:
+            recvPacket, curr_addr = icmpSocket.recvfrom(512)
+            done = True
+            timeRecieved = time.time() * 1000
+            curr_addr = curr_addr[0]
+            try:
+                curr_name = socket.gethostbyaddr(curr_addr)[0]
+            except socket.error:
+                curr_name = curr_addr
+
+            curr_host = "("+curr_name+") - "+"["+curr_addr+"]"
+            sys.stdout.write(str(timeRecieved-timeSent)+"ms ")
+
+        except socket.error:
+            tries-=1
+            sys.stdout.write("* ")
+            curr_host = curr_addr
+    '''
+
+    while not done and tries > 0:
+        ready = select.select([icmpSocket], [], [], timeout)
+
+        if ready[0] == []:
+            sys.stdout.write("* ") # timeout
+            tries-=1
+            continue
+
+        recvPacket, curr_addr = icmpSocket.recvfrom(512)
+        done = True
+        timeRecieved = time.time() * 1000
+        curr_addr = curr_addr[0]
+
+        try:
+            curr_name = socket.gethostbyaddr(curr_addr)[0]
+        except socket.error:
+            curr_name = curr_addr
+    
+        curr_host = "("+curr_name+") - "+"["+curr_addr+"]"
+        sys.stdout.write(str(timeRecieved-timeSent)+"ms ")
+    
+        
+    sys.stdout.write(str(curr_host)+"\n")
+    return curr_addr
+    
+def sendOnePing(icmpSocket, destinationAddress, ID):
+    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, 0, ID, 1)
+
+    chkSum = checksum(header)
+
+    packet = struct.pack('bbHHh', ICMP_ECHO_REQUEST, 0, chkSum, ID, 1)
+
+    icmpSocket.sendto(packet, (destinationAddress, PORT))
+
+    return time.time() * 1000
+    
+def doOnePing(destinationAddress, timeout, ttl, protocol): 
+    protocol = socket.getprotobyname(protocol)
+
+    # UDP protocol
+    if (protocol == 17):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, protocol)
+    else: # ICMP protocol
+        s = socket.socket(socket.AF_INET, socket.SOCK_RAW, protocol) 
+    s.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
+
+    # GNU timout struct
+    #timeoutByte = struct.pack("ll", timeout, 0)
+    r = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.getprotobyname("ICMP"))
+    r.settimeout(timeout)
+    #r.setblocking(True)
+    #r.setsockopt(socket.SOL_SOCKET, socket.SO_RCVTIMEO, timeoutByte)
+    #r.bind(("", PORT))
+
+
+    sys.stdout.write(str(ttl)+" ")
+
+    ID = os.getpid() & 0xFFFF
+
+    timeSent = sendOnePing(s, destinationAddress, ID)
+    curr_addr = receiveOnePing(r, destinationAddress, ID, timeout, timeSent)
+
+    s.close()
+    return curr_addr
+
+def ping(host, timeout=1, protocol="udp"):
+    try:
+        ip = socket.gethostbyname(host)
+    except:
+        print("IP not found for address: "+host)
+        return
+
+    print("Tracing route to '"+host+"' over a maximum of "+str(MAX_HOPS)+" hops:")
+
+    curr_addr = None
+    ttl = 1
+    while curr_addr != ip and ttl < MAX_HOPS:
+        curr_addr = doOnePing(ip,timeout, ttl, protocol)
+        ttl+=1
+    
+
+#ping("localhost", 5)
+#ping("anifox.moe", 1)
+#ping("lancaster.ac.uk", 5)
+ping("google.co.uk",1, "icmp")
+ping("google.co.uk",1, "udp")
+
